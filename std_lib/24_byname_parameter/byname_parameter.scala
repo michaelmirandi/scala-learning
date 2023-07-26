d66128ba9da6c9a4b1f2802335be22e325356b70
michaelmirandi <mmirandi@vt.edu>
2023-07-25 21:34:57 -0400

    :add byname parameter

---
+++ std_lib/24_byname_parameter/byname_parameter.scala
@@ -0,0 +1,46 @@
+object PigLatinizer { 
+    def apply(x: => String) = x.tail + x.head + "ay"
+}
+
+object BynameParameter {
+    def calc(x: () => Int): Either[Throwable, Int] = {
+        try Right(x())
+        catch {
+            case b: Throwable => Left(b)
+        }
+    }
+
+    def anotherCalc(x: => Int): Either[Throwable, Int] = {
+        try Right(x)
+        catch {
+            case b: Throwable => Left(b)
+        }
+    }
+
+    def main(args: Array[String]) = {
+        println("Hello byname parameter")
+        // () => Int is a Function type that takes a Unit type
+        // Unit is known as void to a Java programmer
+        val y = calc{() => 14 + 15 }
+
+        println(y)
+
+        // by-name param does the same thing,
+        // but no need to explicitly handle the Unit or ()
+        val anotherY = anotherCalc {
+            println("Here we go!!!")
+            val z = List(1, 2, 3, 4)
+            49 + 20
+        }
+
+        println(anotherY)
+
+        val result = PigLatinizer {
+            val x = "pret"
+            val z = "zel"
+            x ++ z
+        }
+
+        println(result)
+    }
+}
