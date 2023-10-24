b2bc4a0bcfff42ee3083deeb7fa9953c866c8e06
michaelmirandi <61252938+michaelmirandi@users.noreply.github.com>
2023-10-23 21:05:31 -0400

    :add reference vs. value types explaination

--- std_lib/29_empty_values/empty_values.scala
+++ std_lib/29_empty_values/empty_values.scala
@@ -57,5 +57,25 @@ object EmptyValues {
 
         println(yetAnotherOption.getOrElse("No Value"))
         println(yetAnotherOptionAgain.getOrElse("No Value"))
+
+        /*
+            Value types:
+                Basic data types that represent simple values (int, double, char, boolean)
+                Stored on the stack & passed by value
+                When you pass a value type to a function, you're passing a copy of that value
+                Don't support methods and fields in the traditional OOP sense
+
+            Reference types:
+                Represent objects
+                All custom, user-defined types, like classes, traits, and objects are reference types
+                Stored on the heap
+                Passing a reference type to a function passes the pointer, not a copy of the object
+                String, List, Array, and all user-defined class and object instances
+
+            Value classes can provide a middle ground by allowing more efficient user-defined
+            types in certain scenarios 
+
+            
+        */
     }
 }
